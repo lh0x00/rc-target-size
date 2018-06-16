045e27097ae1fb8bc98230339b60311f98a4f019
Lam Hieu <lamhieu.vk@gmail.com>
2018-06-16 18:55:04 +0700

    update example

--- examples/component.js
+++ examples/component.js
@@ -3,33 +3,13 @@
 import React from 'react'
 import { TargetSize } from 'rc-target-size'
 
-const ResizeOfMe = ({ width, height }) => (<div>component - size of me: {width}x{height}</div>)
+const ResizeOfMe = ({ width, height }) => (<div style={{ height: 100 }}>component - size of me: {width}x{height}</div>)
 
 const App = () => (
   <div>
-    <div style={{ height: '100%', width: '100%' }}>
-      <TargetSize>
-        <ResizeOfMe />
-      </TargetSize>
-    </div>
-
-    <div style={{ height: '100%', width: 250 }}>
-      <TargetSize>
-        <ResizeOfMe />
-      </TargetSize>
-    </div>
-
-    <div style={{ height: '100%', width: 500 }}>
-      <TargetSize>
-        <ResizeOfMe />
-      </TargetSize>
-    </div>
-
-    <div style={{ height: '100%', width: 750 }}>
-      <TargetSize>
-        <ResizeOfMe />
-      </TargetSize>
-    </div>
+    <TargetSize>
+      <ResizeOfMe />
+    </TargetSize>
   </div>
 )
 
--- examples/hoc.js
+++ examples/hoc.js
@@ -3,27 +3,13 @@
 import React from 'react'
 import { targetSize } from 'rc-target-size'
 
-const ResizeOfMeWrapped = ({ width, height }) => (<div>hoc - size of me: {width}x{height}</div>)
+const ResizeOfMeWrapped = ({ width, height }) => (<div style={{ height: 100 }}>hoc - size of me: {width}x{height}</div>)
 
 const ResizeOfMe = targetSize()(ResizeOfMeWrapped)
 
 const App = () => (
   <div>
-    <div style={{ height: '100%', width: '100%' }}>
-      <ResizeOfMe />
-    </div>
-
-    <div style={{ height: '100%', width: 250 }}>
-      <ResizeOfMe />
-    </div>
-
-    <div style={{ height: '100%', width: 500 }}>
-      <ResizeOfMe />
-    </div>
-
-    <div style={{ height: '100%', width: 750 }}>
-      <ResizeOfMe />
-    </div>
+    <ResizeOfMe />
   </div>
 )
 
