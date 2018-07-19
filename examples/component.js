91a93afa796b968a2cea7949575d3be5a157b222
Lam Hieu <lamhieu.vk@gmail.com>
2018-07-19 13:57:41 +0700

    format examples

--- examples/component.js
+++ examples/component.js
@@ -3,7 +3,11 @@
 import React from 'react'
 import { TargetSize } from 'rc-target-size'
 
-const ResizeOfMe = ({ width, height }) => (<div style={{ height: 100 }}>component - size of me: {width}x{height}</div>)
+const ResizeOfMe = ({ width, height }) => (
+  <div style={{ height: 100 }}>
+    component - size of me: {width}x{height}
+  </div>
+)
 
 const App = () => (
   <div>
