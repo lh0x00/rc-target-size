dff9585eab3e0be9b37ee1351bf38bef0b8f2be7
Lam Hieu <lamhieu.vk@gmail.com>
2018-08-20 13:53:41 +0700

    add render child function test file

---
+++ test/TargetSize/renderChildFn.test.js
@@ -0,0 +1,36 @@
+/** eslint-disable */
+
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import TargetSize from 'components/TargetSize'
+import { result } from 'test/TargetSize/data'
+
+describe('TargetSize - render component', () => {
+  const Wrapped = () => <div>{result.text}</div>
+  const Component = () => (
+    <TargetSize>{({ ...props }) => <Wrapped {...props} />}</TargetSize>
+  )
+
+  it('have wrappers', () => {
+    const component = mount(<Component />)
+    expect(component.exists('TargetSize')).toEqual(true)
+    expect(component.exists('TargetWrapper')).toEqual(true)
+    expect(component.exists('TargetReference')).toEqual(true)
+    expect(component.exists('Wrapped')).toEqual(true)
+  })
+
+  it('render content', () => {
+    const component = mount(<Component />)
+    expect(component.text()).toEqual(result.text)
+  })
+
+  it('have props', () => {
+    const component = mount(<Component />)
+    expect(component.find('Wrapped').props()).toEqual(result.props)
+  })
+
+  it('render in server-side', () => {
+    const component = shallow(<Component />)
+    expect(component.html()).toEqual(result.html)
+  })
+})
