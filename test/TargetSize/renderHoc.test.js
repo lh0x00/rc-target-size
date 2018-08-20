7be4d69fd58d7941ec1fc7db38ea31c3874d5b8b
Lam Hieu <lamhieu.vk@gmail.com>
2018-08-20 13:53:29 +0700

    add render hoc test file

---
+++ test/TargetSize/renderHoc.test.js
@@ -0,0 +1,35 @@
+/** eslint-disable */
+
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import targetSize from 'hoc/targetSize'
+import { result } from 'test/TargetSize/data'
+
+describe('TargetSize - render hoc', () => {
+  const Wrapped = ({ width, height }) => <div>{result.text}</div>
+
+  const Component = targetSize()(Wrapped)
+
+  it('have wrappers', () => {
+    const component = mount(<Component />)
+    expect(component.exists('TargetSize')).toEqual(true)
+    expect(component.exists('TargetWrapper')).toEqual(true)
+    expect(component.exists('TargetReference')).toEqual(true)
+    expect(component.exists('Wrapped')).toEqual(true)
+  })
+
+  it('render content', () => {
+    const component = mount(<Component />)
+    expect(component.text()).toEqual(result.text)
+  })
+
+  it('have props', () => {
+    const component = mount(<Component />)
+    expect(component.find('Wrapped').props()).toEqual(result.props)
+  })
+
+  it('render in server-side', () => {
+    const component = shallow(<Component />)
+    expect(component.html()).toEqual(result.html)
+  })
+})
