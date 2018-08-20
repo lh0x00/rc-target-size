a6bef5acf217b0bc76868a64eff64434fbd25907
Lam Hieu <lamhieu.vk@gmail.com>
2018-08-20 13:40:20 +0700

    test component case render component

---
+++ test/TargetSize/data.js
@@ -0,0 +1,10 @@
+export const result = {
+  html: '<div>_content_</div>',
+  text: '_content_',
+  props: {
+    canUseDOM: false,
+    width: 0,
+    height: 0,
+    offset: { x: 0, y: 0 },
+  },
+}
